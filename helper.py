2a28ffabb96e77ad62403541749a022c7b96578b
gianpc <gpaucarcortez@gmail.com>
2026-05-23 13:13:13 -0500

    lab06: expandir texto y temperature completado - helper.py actualizado

--- helper.py
+++ helper.py
@@ -5,11 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 client = OpenAI()
 
-def get_completion(prompt, model="gpt-3.5-turbo"):
+def get_completion(prompt, model="gpt-3.5-turbo", temperature=0):
     messages = [{"role": "user", "content": prompt}]
     response = client.chat.completions.create(
         model=model,
         messages=messages,
-        temperature=0,
+        temperature=temperature,
     )
     return response.choices[0].message.content
